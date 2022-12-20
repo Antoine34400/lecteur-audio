1d5aa3d895b243397d50d055812c732524999e1e
Antoine <antoine@foret-retrouvee.com>
2022-12-20 14:22:44 +0300

    changement css et html commencement js

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
-let value = document.querySelector('#remarque_user');
+const inputRemark = document.querySelector('input[type = "text"]');
+let remarque = "";
 let btn_ = document.getElementById('btn_');
 
-btn_.addEventListener('input', (e) => {
-    value = e.target.value;
-    document.querySelector('#admin').innerHTML('Remarque : '+`$value`)
-})
+inputRemark.addEventListener('input', (e) => {
+    remarque = e.target.value;
+    
+});
 
 const header = document.querySelector('#header');
 
-
-
 window.addEventListener('scroll', (e) => {
-    console.log(e);
+
     if (window.scrollY > 90) {
         header.style.top = 0;
     }
