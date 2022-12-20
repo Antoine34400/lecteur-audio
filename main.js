1549f9927653c5c603a82c692addb649bc47c4d7
Antoine <antoine@foret-retrouvee.com>
2022-12-20 12:36:16 +0300

    ajustement css et html

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
 let value = document.querySelector('#remarque_user');
-
 let btn_ = document.getElementById('btn_');
 
 btn_.addEventListener('input', (e) => {
-    console.log(e);    
+    value = e.target.value;
+    document.querySelector('#admin').innerHTML('Remarque : '+`$value`)
 })
+
+const header = document.querySelector('#header');
+
+
+
+window.addEventListener('scroll', (e) => {
+    console.log(e);
+    if (window.scrollY > 90) {
+        header.style.top = 0;
+    }
+    else{
+        header.style.top = '-500px';
+    }
+})
+
